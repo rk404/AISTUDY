c662210a3adcd1399f6b552a5cb7ed6586744657
Rafał Kamiński <rafalkaminski6@gmail.com>
2025-04-24 10:36:49 +0200

    test12

---
+++ python/test2.py
@@ -0,0 +1,18 @@
+slownik = {1: 'jeden', 'dwa': 2, 3.5: False}
+
+for i in slownik.keys(): 
+    print(i)
+
+slownik = {1: 'jeden', 'dwa': 2, 3.5: False}
+for i in slownik.values():
+    print(i)
+
+
+slownik = {1:'jeden',
+ 2:'dwa',
+ 3:'trzy',
+ 4:'cztery'}
+nowy_slownik = {klucz:wartosc for (klucz,wartosc) in slownik.items() if klucz <=3}
+print(nowy_slownik)
+
+
